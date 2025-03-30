d60bb9889e353a3962b6f9fe841fc8d1af9cbcec
Victor Augusto <victor.macc12@gmail.com>
2025-03-30 17:01:57 -0300

    create selenium_elements

---
+++ web_scraper/utils/selenium_elements.py
@@ -0,0 +1,7 @@
+
+from selenium.webdriver.common.by import *
+
+
+cokkies_button_e = (By.CSS_SELECTOR, "body > div.dsgov > div > div > div > div > " "div.br-modal-footer.actions > button.br-button"".secondary.small.btn-accept")
+procedures_e = (By.CSS_SELECTOR, "#cfec435d-6921-461f-b85a-b425bc3cb4a5 > div > ol > li:nth-child(1) > a:nth-child(1)")
+download_annex_e = (By.CSS_SELECTOR, "#cfec435d-6921-461f-b85a-b425bc3cb4a5 > div > ol > li:nth-child(1) > a:nth-child(1)")
